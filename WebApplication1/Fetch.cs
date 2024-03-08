7ac494dca090a74eb3cffe731abf88cf2e300f9a
Vadiraj Joshi <vadirajjoshi@microsoft.com>
2024-03-08 11:53:55 +0530

    Add project files.

---
+++ WebApplication1/Fetch.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1
+{
+    public class Fetch
+    {
+        public string ScrollText { get; set; }
+
+        public async Task<string> GetData()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://techconcs1.blob.core.windows.net/testcontainer01/openinggrawl.txt");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string ScrollText { get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            Fetch fetch = new Fetch();
+            ScrollText = fetch.GetData().Result;
+        }
+    }
+}
